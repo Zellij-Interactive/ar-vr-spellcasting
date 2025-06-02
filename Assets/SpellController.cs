f155bbea24f5b8390a4981bd826760ef7952632c
Karim Ouhammi <48060593+karim-ouhammi@users.noreply.github.com>
2025-06-02 19:54:37 +0200

    add SpellController class

---
+++ Assets/SpellController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class SpellController : MonoBehaviour
+{
+    public void CastSpell(string spellName)
+    {
+        Debug.Log("Casting: " + spellName);
+        // TODO: Add spell logic
+    }
+}
